aeb13bd293d396da572b2d1b805a41dd426464ba
Sai Vamshitha <saivamshitha@gmai.com>
2025-12-15 12:01:15 +0530

    Initial React app

--- src/Mycomponents/Classroom.js
+++ src/Mycomponents/Classroom.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import './Classroom.css'; 
-import classroom from './images/classroom.webp';
-import classroom1 from './images/classroom1.webp';
+import React from "react";
+import "./Classroom.css";
+
+import classroom from "./images/classroom.webp";
+import classroom1 from "./images/classroom1.webp";
 
 const Classroom = () => {
   return (
     <div className="learning-modes-container">
+
+      {/* Online Live Card */}
       <div className="mode-card">
-        
-        <h3 className="mode-title">Live Online</h3>
+        <h3 className="mode-title">Online Live</h3>
         <p className="mode-description">
-          Attend interactive live online instructor-led classes from the comfort of your home
+          Attend interactive live online instructor-led classes from the comfort of your home.
         </p>
-      
-        <img src={classroom1} alt="./images/classroom1.webp" className="mode-image" />
+        <img src={classroom1} alt="Online Live Class" className="mode-image" />
       </div>
 
+      {/* Classroom Card */}
       <div className="mode-card">
-      
         <h3 className="mode-title">Classroom</h3>
         <p className="mode-description">
-          Learn physically at Vision computers at kodad also learn hands on experience.
+          Learn physically at Vision Computers, Kodad, and gain hands-on experience.
         </p>
-         
-        <img src={classroom} alt="./images/classroom.webp" className="mode-image" />
+        <img src={classroom} alt="Classroom Training" className="mode-image" />
       </div>
+
     </div>
   );
 };
